96c04368a46726029c5af807c6ef8c14ff7e22da
Mohamad Kabalan <mohamad.kabalan@standingongiants.com>
2026-05-28 09:37:53 +0200

    First Commit

--- config/database.php
+++ config/database.php
@@ -44,11 +44,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => 'InnoDB',
-            'options' => extension_loaded('pdo_mysql')
-                ? array_filter([
-                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                ])
-                : [],
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+              Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'pgsql' => [
